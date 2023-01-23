9a0ad1be4f8b25de934ece8895ab1df353af5704
ucheobioma <47930447+ucheobioma@users.noreply.github.com>
2023-01-23 10:11:42 +0100

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -6,18 +6,18 @@ from pathlib import Path
 html = Template(Path('template.html').read_text())
 # we want to read the text contained in the html file
 email = EmailMessage()  # create an email object
-email['from'] = 'uche'
-email['to'] = 'ucheobioma45@gmail.com'
+email['from'] = #name
+email['to'] = #receipient email
 email['subject'] = 'YOU CAN NOW SPAM PEOPLE'
-email.set_content(html.substitute({'name': "uche"}), 'html')
+email.set_content(html.substitute({'name': name}), 'html')
 
 
-# email.set_content('Ride on uche...youre doing yahoo now lol')
+# email.set_content('Ride on ...youre doing yahoo now lol')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()  # to alert the sever and establish connection
     smtp.starttls()  # this is an encryption mechanism for connecting securely
-    smtp.login('viculstudio@gmail.com', 'oymzyxcewmscgjal')
+    smtp.login('sender_email', generated_key_from_emailing_service)
     smtp.send_message(email)
     print('looks good')
 
